bd3d85d0ef13405d4f3e1665537ffc523a231130
ornk <ornk@kth.se>
2018-04-06 16:20:24 +0200

    Created Store Map

--- ShopProject/Contact.aspx
+++ ShopProject/Contact.aspx
@@ -9,9 +9,20 @@
         <abbr title="Phone">P:</abbr>
         425.555.0100
     </address>
+    <Border
+        BorderThickness="2"
+        BorderBrush="Black"
+        Background="LightGray"
+        HorizontalAlignment="Left"
+        VerticalAlignment="Top"
+        Width="100"
+        Height="100">
+            <canvas id="myCanvas" width="500" height="500" style="border:1px solid #000000;"></canvas>
+    </Border>
 
     <address>
         <strong>Support:</strong>   <a href="mailto:Support@example.com">Support@example.com</a><br />
         <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
     </address>
+    <script type ="text/javascript" src ="../Scripts/storeMap.js"></script>
 </asp:Content>
---
+++ ShopProject/Scripts/storeMap.js
@@ -0,0 +1,66 @@
+﻿var rackLength = 50;
+var rackHeight = 15;
+
+function drawStore() {
+
+    drawAllRacks();
+    drawDoors();
+
+}
+
+function drawDoors() {
+    //TODO
+}
+
+function drawAllRacks() {
+    var x = 75;
+    var y = 75;
+
+    for (var i = 0; i < 5; i++) {
+        console.log("hello!");
+        console.log(y);
+        drawRack(x, y, 2, 3);
+        y += 55;
+        console.log(y);
+    }
+    y = 50;
+    x = 300;
+    for (i = 0; i < 8; i++) {
+        drawRack(x, y, 2, 3);
+        y += 55;
+    }
+}
+
+function drawRack(x, y, width, height) {
+    var ctx = $('#myCanvas')[0].getContext("2d");
+
+    var xLoc = x;
+    var yLoc = y;
+
+    for (var i = 0; i < width; i++) {
+        for (var j = 0; j < height; j++) {
+            ctx.beginPath();
+            ctx.rect(xLoc, yLoc, rackLength, rackHeight);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = 'black';
+            ctx.stroke();
+            xLoc += rackLength;
+        }
+        xLoc = x;
+        yLoc += rackHeight;
+    }
+
+}
+
+drawStore();
+
+
+////get a reference to the canvas
+//var ctx = $('#myCanvas')[0].getContext("2d");
+
+
+////draw a circle
+//ctx.beginPath();
+//ctx.arc(85, 85, 10, 0, Math.PI * 2, true);
+//ctx.closePath();
+//ctx.fill();
